0c470ce2b56ee34f713e5508475e48396d9015cd
bbauer <brian.bauer@g5platform.com>
2012-11-17 19:04:09 -0800

    Add model for email addresses

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,4 +1,3 @@
 class Email < ActiveRecord::Base
-  belongs_to :customer
   attr_accessible :email
 end
--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Email do
-  it{ should belong_to(:customer) }
+  pending "add some examples to (or delete) #{__FILE__}"
 end
