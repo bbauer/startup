7415c339aa4782c10ba386173532066421b9dde3
bbauer <brian.bauer@g5platform.com>
2012-11-17 17:22:29 -0800

    Explicitly configure and precompile active admin

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,6 @@ module Service
     config.assets.version = '1.0'
 
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += ['active_admin.css.scss', 'active_admin.js']
   end
 end
